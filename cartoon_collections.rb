4e461aab48c57538a69c3d94f55cc6b2f452c2d6
Faith Janicek <faithe1937@gmail.com>
2019-10-10 22:49:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ def long_planeteer_calls(array)
   i = 0 
   if array.any? {|word| word.length > 4}
     return true 
-  else return false 
+  else 
+    return false 
+  end 
 end
 
 def find_the_cheese# code an argument here
